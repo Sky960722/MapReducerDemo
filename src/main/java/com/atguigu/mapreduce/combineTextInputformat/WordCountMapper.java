9db68abc61aa0ee7c121c4e64991f65559accf24
sry <1448131306@qq.com>
2022-02-19 22:21:08 +0800

    writable commit

---
+++ src/main/java/com/atguigu/mapreduce/combineTextInputformat/WordCountMapper.java
@@ -0,0 +1,35 @@
+package com.atguigu.mapreduce.combineTextInputformat;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+import java.io.IOException;
+
+public class WordCountMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
+
+    private Text outK = new Text();
+    private IntWritable outV = new IntWritable(1);
+
+    @Override
+    protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+        //1 获取一行
+        String line = value.toString();
+
+        //2 切割
+        String[] words = line.split(" ");
+
+        //3 循环写出
+        for (String word : words) {
+
+            //封装
+            outK.set(word);
+
+            //输出
+            context.write(outK,outV);
+
+
+        }
+    }
+}
