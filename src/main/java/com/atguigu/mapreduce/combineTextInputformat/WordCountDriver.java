9e1f7ac8f3419176a088defa37c1e61be4fbdee4
sry <1448131306@qq.com>
2022-02-20 14:01:25 +0800

    combine commit

--- src/main/java/com/atguigu/mapreduce/combineTextInputformat/WordCountDriver.java
+++ src/main/java/com/atguigu/mapreduce/combineTextInputformat/WordCountDriver.java
@@ -5,6 +5,7 @@ import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.CombineTextInputFormat;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
@@ -27,13 +28,19 @@ public class WordCountDriver {
         job.setMapOutputKeyClass(Text.class);
         job.setMapOutputValueClass(IntWritable.class);
 
+        //如果不设置InputFormat，它默认用的是TextInputFormat.class
+        job.setInputFormatClass(CombineTextInputFormat.class);
+
+        //虚拟存储切片最大值设置4m
+        CombineTextInputFormat.setMaxInputSplitSize(job,20971520);
+
         //5 设置最终输出的kv类型
         job.setOutputKeyClass(Text.class);
         job.setOutputValueClass(IntWritable.class);
 
         //6 设置输入路径和输出路径
-        FileInputFormat.setInputPaths(job, new Path("D:\\input\\inputword"));
-        FileOutputFormat.setOutputPath(job, new Path("D:\\output\\output1"));
+        FileInputFormat.setInputPaths(job, new Path("D:\\input\\inputcombinetextinputformat"));
+        FileOutputFormat.setOutputPath(job, new Path("D:\\output\\output777"));
 
         //7 提交job
         boolean result = job.waitForCompletion(true);
