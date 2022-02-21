c86f44819641aabb109212b748b1117ad7529c9a
sry <1448131306@qq.com>
2022-02-21 21:21:19 +0800

    MapperJoin commit

---
+++ src/main/java/com/atguigu/mapreduce/reduceJoin/TableReducer.java
@@ -0,0 +1,49 @@
+package com.atguigu.mapreduce.reduceJoin;
+
+import org.apache.commons.beanutils.BeanUtils;
+import org.apache.hadoop.io.NullWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.util.ArrayList;
+
+public class TableReducer extends Reducer<Text, TableBean, TableBean, NullWritable> {
+    @Override
+    protected void reduce(Text key, Iterable<TableBean> values, Reducer<Text, TableBean, TableBean, NullWritable>.Context context) throws IOException, InterruptedException {
+
+        ArrayList<TableBean> orderBeans = new ArrayList<>();
+        TableBean pdBean = new TableBean();
+        for (TableBean value : values) {
+
+            if("order".equals(value.getFlag())){
+                TableBean tmpTableBean = new TableBean();
+                try {
+                    BeanUtils.copyProperties(tmpTableBean,value);
+                } catch (IllegalAccessException e) {
+                    e.printStackTrace();
+                } catch (InvocationTargetException e) {
+                    e.printStackTrace();
+                }
+
+                orderBeans.add(tmpTableBean);
+            }else{
+                try {
+                    BeanUtils.copyProperties(pdBean,value);
+                } catch (IllegalAccessException e) {
+                    e.printStackTrace();
+                } catch (InvocationTargetException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        for (TableBean orderBean : orderBeans) {
+            orderBean.setPname(pdBean.getPname());
+
+            context.write(orderBean,NullWritable.get());
+        }
+    }
+}
